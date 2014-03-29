aff64f4e2a426dae95fef91e83af7c407df9f92b
sumakumardey <bappi.5476@gmail.com>
2014-03-29 14:39:25 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataMonster::Application.config.secret_token = 'aae6226b468a21a222ec27e414c60f2000e0616f857b07db3c4c69dd5bda92d784499662e9bf8a57bd68ce0371cab0c3750a409809b6f05b8f5d8c348933118a'
