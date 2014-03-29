76aae108ff2560f66d043733ebeef918dfd652cc
sumakumardey <bappi.5476@gmail.com>
2014-03-30 01:13:31 +0530

    automater

--- lib/generator/automater.rb
+++ lib/generator/automater.rb
@@ -8,12 +8,13 @@ module Generator
         current_status = booking_status
         while number_of_times > 0 do
           date = data.boarding_date - number_of_times.days
-          current_status = (current_status-r.rand(0..10))
-          current_booking = ((current_status>0)? "W/L #{current_status}" : "CNF")
+          current_status = (current_status-r.rand(0..15))
+          current_booking = ((current_status>1)? "W/L #{current_status}" : "CNF")
           PassengerStatusGen.create(:pnr_table_id => data.id,
                                :pnr_number => data.pnr_number,
                                :booking_status => booking_status,
-                               :current_status => current_booking)
+                               :current_status => current_booking,
+                               :updated_at => date )
           break if current_booking == "CNF"
           current_booking = ""
           number_of_times = number_of_times - 1
